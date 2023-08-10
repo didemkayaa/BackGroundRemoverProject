f83a6a18e6324dddcd8739528ce2300a6141c653
Didem Kaya <didemkaya043@gmail.com>
2023-08-10 14:29:31 +0300

    first line added2

---
+++ main.py
@@ -0,0 +1,12 @@
+from rembg import remove
+
+input_path = "image2.jpg"
+out_path = "output.png"
+
+with open(input_path, 'rb') as i:
+    with open(out_path, 'wb') as o:
+        input_file=i.read()
+        output_file=remove(input_file)
+        o.write(output_file)
+
+
